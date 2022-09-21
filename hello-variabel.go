6763c6c430665a6f8b3dd4ce5d36f3b5aa42255e
hpnabila03 <113921858+hpnabila03@users.noreply.github.com>
2022-09-21 02:12:08 -0800

    Add files via upload

---
+++ hello-variabel.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name:= "nabila ghaisani hendrika putri"
+	var age:= 23 
+	var adress:= "Medan Sumatera Utara"
+
+fmt.printf("halo namaku nabila ghaisani, umurku adalah 23, aku tinggal di Medan Sumatera Utara" name, age, adress)
+
+}
